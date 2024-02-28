5cd9886047e8505f39dd0b4594ba0035a33a87bc
Juan Felipe Rubio Sanabria <152001380+RubioJuan@users.noreply.github.com>
2024-02-28 16:49:29 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,35 +2,35 @@ const personajes = [
     {
         "id": "Spiderman",
         "about": "Ipsum tempor sunt Lorem est. Fugiat nisi velit veniam labore et ullamco minim adipisicing do culpa. Cillum voluptate reprehenderit aute consectetur.",
-        "picture": "https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png",
+        "picture": "https://static.tvtropes.org/pmwiki/pub/images/amazing_spider_man_vol_2_50_textless.png",
     "squarePic": "https://static.wikia.nocookie.net/ficcion-sin-limites/images/6/6b/Spidey.png/revision/latest/scale-to-width-down/375?cb=20210226124716&path-prefix=es",
         "name": "Spiderman"
     },
     {
         "id": "Iroman",
         "about": "Deserunt voluptate aliquip ex dolor Lorem exercitation aliqua nisi fugiat aliquip sunt non. Eu cillum enim velit exercitation officia proident exercitation ipsum exercitation Lorem nulla do. Minim sint dolor nostrud ipsum laborum. Velit ea ad ad consectetur nisi Lorem laborum officia esse. Do eu incididunt eiusmod voluptate excepteur consequat ipsum ad. Quis veniam exercitation eiusmod amet non non eu aliquip quis ea. Tempor deserunt nulla adipisicing qui fugiat ipsum labore duis et ea consectetur.",
-        "picture": "https://www.geekmi.news/__export/1698454800382/sites/debate/img/2023/10/24/disexo_sin_txtulo_x45x.jpg_423682103.jpg",
+        "picture": "https://i0.wp.com/photos1.blogger.com/blogger/656/2170/1600/Iron%20Man.jpg",
     "squarePic": "https://static.wikia.nocookie.net/disney/images/9/96/Iron-Man-AOU-Render.png/revision/latest/scale-to-width-down/1200?cb=20180410032118&path-prefix=es",
         "name": "Iroman"
     },
     {
         "id": "Capitan America",
         "about": "Esse magna Lorem ipsum incididunt sit. Enim eiusmod proident in adipisicing anim excepteur laborum magna aliqua adipisicing ad qui aliqua. Eiusmod enim ullamco laboris adipisicing tempor sint labore. Exercitation cupidatat incididunt nostrud velit nisi aute eu ex pariatur deserunt mollit. Deserunt nisi enim irure elit. Aute amet amet consequat laborum dolor sint anim Lorem id fugiat commodo cupidatat ad. Ipsum quis enim consectetur occaecat laboris esse.",
-        "picture": "https://pm1.aminoapps.com/6072/a6accc9fef58647c3aa6607a712b5e6bfefa07d4_hq.jpg",
+        "picture": "https://www.akiracomics.com/imagenes/poridentidad?identidad=c1c19372-97ca-4fef-8d35-c9cb19fbcc01&ancho=850&alto=",
     "squarePic": "https://static.wikia.nocookie.net/disney/images/f/fa/Captain-America-AOU-Render.png/revision/latest?cb=20180420015558&path-prefix=es",
         "name": "Capitan America"
     },
     {
         "id": "Thor",
         "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://i.pinimg.com/736x/ac/96/91/ac96918f842e8ef6595ae192adf76fce.jpg",
+        "picture": "https://pm1.aminoapps.com/6857/2a033ee70f8ad81c35510cb13a546a196175597dv2_hq.jpg",
     "squarePic": "https://static.wikia.nocookie.net/personajes-de-ficcion-database/images/c/cb/Classic_Thor.png/revision/latest/scale-to-width-down/519?cb=20201003125917&path-prefix=es",
         "name": "Thor"
     },
     {
         "id": "Doctor Strange",
         "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://revistahush.com/wp-content/uploads/2023/04/Doctor-Stange-7.webp",
+        "picture": "https://i.pinimg.com/564x/02/c5/f0/02c5f06bd86d14be48e896a88e02a6b8.jpg",
     "squarePic": "https://cdn.kinocheck.com/i/3e1r3zcsy8.jpg",
         "name": "Doctor Strange"
     }, {
@@ -43,14 +43,14 @@ const personajes = [
     {
         "id": "Deadpool",
         "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/424b4e7e-df3c-4d76-a996-927cced18d13/devksi3-dd8abcd4-d3c0-48d7-a5ec-f9146bbbda13.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQyNGI0ZTdlLWRmM2MtNGQ3Ni1hOTk2LTkyN2NjZWQxOGQxM1wvZGV2a3NpMy1kZDhhYmNkNC1kM2MwLTQ4ZDctYTVlYy1mOTE0NmJiYmRhMTMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.X7mYEDhmVNVvGd3JhEITOgzwfmUnfILIFfWDWmfvrkg",
+        "picture": "https://i.pinimg.com/736x/77/96/50/7796507879438bd7e0e7d978e3198981.jpg",
     "squarePic": "hhttps://static.wikia.nocookie.net/ficcion-sin-limites/images/8/88/DP.png/revision/latest/scale-to-width-down/370?cb=20210217170751&path-prefix=es",
         "name": "Deadpool"
     },
     {
         "id": "Wolverine",
         "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://i0.wp.com/www.tomosygrapas.com/wp-content/uploads/2017/11/wolverine-principal.jpg",
+        "picture": "https://i.pinimg.com/736x/62/cd/66/62cd665950ec45d7a522f1e71e294c77.jpg",
     "squarePic": "https://static.wikia.nocookie.net/featteca/images/8/82/Wolverine_PNG1.png/revision/latest/scale-to-width-down/477?cb=20220219192157&path-prefix=es",
         "name": "Wolverine"
     },
@@ -64,7 +64,7 @@ const personajes = [
     {
         "id": "Pantera Negra",
         "about": "Mollit officia ad excepteur anim proident incididunt eiusmod mollit consectetur id sit velit. Laborum ut magna officia qui laboris eiusmod do culpa. Culpa dolor officia velit cillum culpa deserunt cupidatat cillum ipsum laborum.",
-        "picture": "https://www.smashmexico.com.mx/wp-content/uploads/sites/2/2018/01/12073615/marvel-datos-black-panther-traje.jpg",
+        "picture": "https://external-preview.redd.it/rZ2htb5Cp3IY04dbfZG-U3ZRmehbz7dpavZbq_4vEFk.jpg?auto=webp&s=a9f91f03477a18221d50722139dabb0058909cf0",
     "squarePic": "https://i.pinimg.com/1200x/fa/4b/98/fa4b9861c9bd99c754d3487b3b4637ce.jpg",
         "name": "Pantera Negra"
     },
@@ -85,7 +85,7 @@ const personajes = [
     {
         "id": "Doctor Octopus",
         "about": "Deserunt voluptate aliquip ex dolor Lorem exercitation aliqua nisi fugiat aliquip sunt non. Eu cillum enim velit exercitation officia proident exercitation ipsum exercitation Lorem nulla do. Minim sint dolor nostrud ipsum laborum. Velit ea ad ad consectetur nisi Lorem laborum officia esse. Do eu incididunt eiusmod voluptate excepteur consequat ipsum ad. Quis veniam exercitation eiusmod amet non non eu aliquip quis ea. Tempor deserunt nulla adipisicing qui fugiat ipsum labore duis et ea consectetur.",
-        "picture": "https://e1.pxfuel.com/desktop-wallpaper/657/476/desktop-wallpaper-dr-otto-octavius-doctor-octopus-marvel-comics.jpg",
+        "picture": "https://redwoodartgroup.com/wp-content/uploads/2019/09/DrOctopus_MM2018-scaled.jpg",
     "squarePic": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/335e007c-4af8-4e62-8040-af07c0929c24/dfy8b0l-2a6d7945-7aa4-475a-a893-e6e0ceba0e2f.png/v1/fit/w_375,h_516/doctor_octopus__2021__marvel_puzzle_quest__render_by_egg84_dfy8b0l-375w.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA2NiIsInBhdGgiOiJcL2ZcLzMzNWUwMDdjLTRhZjgtNGU2Mi04MDQwLWFmMDdjMDkyOWMyNFwvZGZ5OGIwbC0yYTZkNzk0NS03YWE0LTQ3NWEtYTg5My1lNmUwY2ViYTBlMmYucG5nIiwid2lkdGgiOiI8PTc3NSJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.Fkt7C-mXIKtkS4KXDI3yInyajJELRATffzl9PtoWFiQ",
         "name": "Doctor Octopus"
     },
@@ -110,7 +110,7 @@ const personajes1 = [
     {
         "id": "Batman",
         "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://www.geekmi.news/__export/1604437633818/sites/debate/img/2020/11/03/batman.jpg_423682103.jpg",
+        "picture": "https://i.pinimg.com/736x/d8/ab/93/d8ab936f4417dc06ac16af7f15e94508.jpg",
     "squarePic": "https://i.pinimg.com/736x/57/f1/3e/57f13ec1b6e2d5712d03fec842e08356.jpg",
         "name": "Batman"
     }, 
@@ -131,7 +131,7 @@ const personajes1 = [
     {
         "id": "Saint Walker",
         "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://2.bp.blogspot.com/--j_Ycdq4cf4/WWE-42jjd_I/AAAAAAAANrg/U6OOY_qXFFkWgRKiYn1QskYRrI5CIEdHACLcBGAs/s1600/saint-walker.jpg",
+        "picture": "https://pm1.aminoapps.com/6429/474d3eebc9e8f2b1e5fed025bf1ac8f4299ba42a_hq.jpg",
     "squarePic": "https://www.giantbomb.com/a/uploads/scale_medium/16/164924/2455923-walker.png",
         "name": "Saint Walker"
     },
@@ -164,30 +164,30 @@ const personajes1 = [
         "name": "Victor Stone"
     },
     {
-        "id": "Acertijo",
+        "id": "Pinguino",
         "about": "Deserunt voluptate aliquip ex dolor Lorem exercitation aliqua nisi fugiat aliquip sunt non. Eu cillum enim velit exercitation officia proident exercitation ipsum exercitation Lorem nulla do. Minim sint dolor nostrud ipsum laborum. Velit ea ad ad consectetur nisi Lorem laborum officia esse. Do eu incididunt eiusmod voluptate excepteur consequat ipsum ad. Quis veniam exercitation eiusmod amet non non eu aliquip quis ea. Tempor deserunt nulla adipisicing qui fugiat ipsum labore duis et ea consectetur.",
-        "picture": "https://www.looper.com/img/gallery/a-familiar-face-is-being-eyed-to-play-riddler-in-the-batman/intro-1565023656.jpg",
+        "picture": "https://www.akiracomics.com/imagenes/poridentidad?identidad=c75e0021-577d-4de6-ae37-85561adacba0&ancho=850&alto=",
     "squarePic": "https://i.pinimg.com/originals/c8/4c/5a/c84c5af2e49326d2fbb562b29efcbeb7.png",
         "name": "Acertijo"
     },
     {
         "id": "Joker",
         "about": "Esse magna Lorem ipsum incididunt sit. Enim eiusmod proident in adipisicing anim excepteur laborum magna aliqua adipisicing ad qui aliqua. Eiusmod enim ullamco laboris adipisicing tempor sint labore. Exercitation cupidatat incididunt nostrud velit nisi aute eu ex pariatur deserunt mollit. Deserunt nisi enim irure elit. Aute amet amet consequat laborum dolor sint anim Lorem id fugiat commodo cupidatat ad. Ipsum quis enim consectetur occaecat laboris esse.",
-        "picture": "https://media.gq.com.mx/photos/5f467227ef6115112e88bcf9/1:1/w_1514,h_1514,c_limit/multimedia.normal.8f06f5c72b963335.6261746d616e2d74687265652d6a6f6b6572732d323033323430315f6e6f726d616c2e6a7067.jpg",
+        "picture": "https://i.ebayimg.com/images/g/FbEAAOSw9FlfvMZf/s-l1200.webp",
     "squarePic": "https://www.zonanegativa.com/imagenes/2022/11/destacada-deadly.jpg",
         "name": "Joker"
     },
     {
         "id": "Batman que rie",
         "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://i.pinimg.com/564x/1a/79/21/1a7921cfa0a36daf4aed607ac05c82f0.jpg",
+        "picture": "https://arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/RLLN5WFEVNDNXP4MHGRE24TJGA.jpg",
     "squarePic": "https://qph.cf2.quoracdn.net/main-qimg-db0d94f4a533687a72812bd2759d7b1d-lq",
         "name": "Batman que rie"
     },
     {
         "id": "Harley quinn",
         "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://sm.ign.com/t/ign_es/feature/h/how-dcs-ha/how-dcs-harleen-reinvents-harley-quinns-tragic-origin-story_d4kq.1280.jpg",
+        "picture": "https://i.pinimg.com/736x/78/50/fe/7850fefc6bdf25bf951bab0cba545e79.jpg",
     "squarePic": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/77768f52-334f-4ce8-a583-4704f0d5e15f/dgfa05e-ed9efcfe-4b0e-4cef-a89e-26ff13f0f563.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzc3NzY4ZjUyLTMzNGYtNGNlOC1hNTgzLTQ3MDRmMGQ1ZTE1ZlwvZGdmYTA1ZS1lZDllZmNmZS00YjBlLTRjZWYtYTg5ZS0yNmZmMTNmMGY1NjMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.5SIIa3nirrwcY7uKkoP6zPtWUT7VNUz_PM6fC4oeUEs",
         "name": "Harley quinn"
     }, {
@@ -200,7 +200,7 @@ const personajes1 = [
     {
         "id": "Bane",
         "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://larepublica.cronosmedia.glr.pe/migration/images/VUQC7EVEIJHEVM7F4YHPULJGIA.jpg",
+        "picture": "https://1.bp.blogspot.com/-Y27f2bRpwzs/Xt_7EL6NhhI/AAAAAAAB67k/SCNmSLL4xN0Df7ORX1b9kk8_XDzx0vYcwCLcBGAsYHQ/s1600/bane.jpg",
     "squarePic": "https://static.wikia.nocookie.net/dcandmarvelheroes/images/c/ca/317px-Batman_The_Dark_Knight_Vol_2_6_Textless.jpg/revision/latest?cb=20120306220313&path-prefix=es",
         "name": "Bane"
     },
@@ -272,4 +272,65 @@ const contenedor = document.getElementById('contenedor');
 personajes.forEach(personaje => {
     const estructuraHTML = crearEstructuraHTML(personaje);
     contenedor.appendChild(estructuraHTML);
+});
+
+function crearEstructuraHTML(personaje) {
+    const divHola = document.createElement('div');
+    divHola.className = 'hola';
+
+    const divTitulo1 = document.createElement('div');
+    divTitulo1.className = 'titulo1';
+
+    const img = document.createElement('img');
+    img.src = personaje.picture;
+    img.id = personaje.id;
+
+    const br = document.createElement('br');
+
+    const button = document.createElement('button');
+    button.id = 'extra';
+    button.className = 'ver';
+    button.textContent = 'Obtener';
+
+    const divModal = document.createElement('div');
+    divModal.id = 'myModal';
+    divModal.className = 'modalContainer';
+
+    const divModalContent = document.createElement('div');
+    divModalContent.className = 'modal-content';
+
+    const spanClose = document.createElement('span');
+    spanClose.className = 'close';
+    spanClose.textContent = 'X';
+
+    const h2 = document.createElement('h2');
+    h2.textContent = personaje.name;
+
+    const p = document.createElement('p');
+    p.textContent = personaje.about;
+
+    // Adjuntar elementos al árbol DOM
+    divModalContent.appendChild(spanClose);
+    divModalContent.appendChild(h2);
+    divModalContent.appendChild(p);
+
+    divModal.appendChild(divModalContent);
+
+    divTitulo1.appendChild(img);
+    divTitulo1.appendChild(br);
+    divTitulo1.appendChild(button);
+    divTitulo1.appendChild(divModal);
+
+    divHola.appendChild(divTitulo1);
+
+    return divHola;
+}
+
+// Obtener el contenedor donde se agregarán los elementos
+const contenedor1 = document.getElementById('contenedor1');
+
+// Crear y agregar cada estructura HTML al contenedor
+personajes1.forEach(personaje => {
+    const estructuraHTML = crearEstructuraHTML(personaje);
+    contenedor1.appendChild(estructuraHTML);
 });
